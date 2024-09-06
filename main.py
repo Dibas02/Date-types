532b31da6bb1e84c3c377da9a40974f2a095130a
Dibas02 <debal1952@gmail.com>
2024-09-06 19:55:53 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+a = 123
+print("The data type of a is", type(a))
+b = 2.3
+print("The data type of b is", type(b))
+c = True
+print("The data type of c is", type(c))
+d = 'abc'
+print("The data type of d is", type(d))
+
+a = float(a)
+print("The data type of a is", type(a))
+b = int(b)
+print("The data type of b is", type(b))
+c = str(c)
+print("The data type of c is", type(c))
+d = bool(d)
+print("The data type of d is", type(d))
